8a7a578d8bdd050bde3d8b20c5496cf8a0f648bf
Maczeta5 <chmielecki.michall@gmail.com>
2022-08-04 17:38:55 +0200

    todo list

--- js/script.js
+++ js/script.js
@@ -1,24 +1,126 @@
-const currentDay = document.querySelector('.current-day')
-const funFact = document.querySelector('.fun-fact')
+let todoInput //miejsce, gdzie użytkownik wpisuje treść zadania
+let errorInfo //info o braku zadań / konieczności wpisania zadań
+let addBtn //przycisk ADD - dodaje nowe elementy do listy
+let ulList //lista zadań, tagi UL
+let newTodo
 
-const facts = [
-	'Krokodyl nie potrafi wystawić języka.',
-	'Każdy człowiek spędził około pół godziny jako pojedyncza komórka.',
-	'Dźwięk przemieszcza się 15 razy szybciej przez stal niż przez powietrze.',
-	'Leniwce potrzebują dwóch tygodni na strawienie jedzenia.',
-	'Goryle śpią nawet czternaście godzin dziennie.',
-	'Język kameleona jest dwukrotnie dłuższy od jego ciała.',
-	'Chińczycy w ciągu roku zużywają około 80 miliardów pałeczek.',
-	'Żeby wejść na Wieżę Eiffla trzeba pokonać aż 1710 stopni.',
-]
+let popup //popup
+let popupInfo //tekst w popupie, jak się doda pusty teskt
+let todoToEdit // edytowany Todo
+let popupInput //input w popupie
+let popupAddBtn //przycisk 'zatwierdź' w popupie
+let popupCloseBtn //przycisk 'anuluj w popupie
 
-const day = new Date()
-
-currentDay.textContent = day.toLocaleDateString('pl', { weekday: 'long' })
-
-const funFacts = () => {
-	const number = Math.floor(Math.random() * facts.length - 1)
-	funFact.textContent = facts[number]
+const main = () => {
+	prepareDOMElements()
+	prepareDOMEvents()
 }
 
-funFacts()
+const prepareDOMElements = () => {
+	todoInput = document.querySelector('.todo-input')
+	errorInfo = document.querySelector('.error-info')
+	addBtn = document.querySelector('.btn-add')
+	editBtn = document.querySelector('.edit')
+	ulList = document.querySelector('.todolist ul')
+	btnComplete = document.querySelector('.complete')
+	popup = document.querySelector('.popup')
+	popupInfo = document.querySelector('.popup-info')
+	popupInput = document.querySelector('.popup-input')
+	popupAddBtn = document.querySelector('.accept')
+	popupCloseBtn = document.querySelector('.cancel')
+} //funkcj, w której będziemy pobierać wszystkie elementy
+
+const prepareDOMEvents = () => {
+	addBtn.addEventListener('click', addNewTodo)
+	ulList.addEventListener('click', checkClick)
+	popupCloseBtn.addEventListener('click', closePopup)
+	popupAddBtn.addEventListener('click', changeTodoText)
+	todoInput.addEventListener('keyup', enterKeyCheck)
+} //funkcja, w której nadamy nasłuchowanie na elementy
+
+const addNewTodo = () => {
+	if (todoInput.value !== '') {
+		newTodo = document.createElement('li')
+		newTodo.textContent = todoInput.value
+		createToolsArea()
+
+		ulList.append(newTodo)
+
+		todoInput.value = ''
+		errorInfo.textContent = ''
+	} else {
+		errorInfo.textContent = 'Musisz wpisać jakieś zadanie!'
+	}
+}
+
+const createToolsArea = () => {
+	const toolArea = document.createElement('div')
+	toolArea.classList.add('tools')
+	newTodo.append(toolArea)
+
+	const btnComplete = document.createElement('button')
+	btnComplete.classList.add('complete')
+	btnComplete.innerHTML = '<i class="fas fa-check"></i>'
+
+	const btnEdit = document.createElement('button')
+	btnEdit.classList.add('edit')
+	btnEdit.textContent = 'EDIT'
+
+	const btnDelete = document.createElement('button')
+	btnDelete.classList.add('delete')
+	btnDelete.innerHTML = '<i class="fas fa-times"></i>'
+
+	toolArea.append(btnComplete, btnEdit, btnDelete)
+}
+
+const checkClick = e => {
+	if (e.target.matches('.complete')) {
+		e.target.closest('li').classList.toggle('completed')
+		e.target.classList.toggle('completed')
+	} else if (e.target.matches('.edit')) {
+		editTodo(e)
+	} else if (e.target.matches('.delete')) {
+		deleteTodo(e)
+	}
+}
+
+const editTodo = e => {
+	popup.style.display = 'flex'
+	todoToEdit = e.target.closest('li')
+	popupInput.value = todoToEdit.firstChild.textContent
+}
+
+const closePopup = () => {
+	popup.style.display = 'none'
+	popupInfo.textContent = ''
+}
+
+const changeTodoText = () => {
+	if (popupInput.value !== '') {
+		todoToEdit.firstChild.textContent = popupInput.value
+		popup.style.display = 'none'
+		popupInfo.textContent = ''
+	} else {
+		popupInfo.textContent = 'Musisz wprowadzić treść zadania!'
+	}
+}
+
+const deleteTodo = e => {
+	e.target.closest('li').remove()
+	const allTodos = ulList.querySelectorAll('li')
+
+	if (allTodos.length == 0) {
+		errorInfo.textContent = 'Brak zadań na liście...'
+	}
+}
+
+const enterKeyCheck = (e) => {
+if(e.key === 'Enter') {
+	addNewTodo()
+}	
+
+}
+
+
+
+document.addEventListener('DOMContentLoaded', main) //funkcja main uruchomi się dopiero kiedy na naszej stronie wczytaja się wszystkie elementy
